56f816994806526edb51e8e060e8e03a85234cd7
Marcus Francis <marcusmagnusson@outlook.com>
2018-11-15 15:39:56 +0100

    Feat: ListContacts component
    Display contact avatar, name, @, and remove button.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,33 @@
 import React, { Component } from 'react';
+import ListContacts from './ListContacts'
+
+// Default contacts, remove later.
+const contacts = [
+  {
+    "id": "Salmon",
+    "name": "Salmon & Coral",
+    "handle": "@Snapchat",
+    "avatarURL": "http://localhost:5001/salmoncoral.png"
+  },
+  {
+    "id": "Celery",
+    "name": "Mr. Celery",
+    "handle": "@Celerysquash",
+    "avatarURL": "http://localhost:5001/celery.jpg"
+  },
+  {
+    "id": "Grey",
+    "name": "Mr. Grey",
+    "handle": "@Sage",
+    "avatarURL": "http://localhost:5001/grey.jpg"
+  }
+ ];
 
 class App extends Component {
   render() {
     return (
       <div>
-        Hello World
+        <ListContacts contacts={contacts} />
       </div>
     );
   }
