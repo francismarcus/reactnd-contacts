0a174efc81a75688409becc70adff8e501580052
Marcus Francis <marcusmagnusson@outlook.com>
2018-11-25 04:01:40 +0100

    refactor: ListContacts component
    Convert ListContacts to a Stateless Functional Component

--- src/ListContacts.js
+++ src/ListContacts.js
@@ -1,29 +1,22 @@
-import React, { Component } from 'react'
+import React from 'react'
 
-class ListContacts extends Component {
-    render() {
-        return ( 
-            <ol className = 'contact-list' >
-                {this.props.contacts.map((contact) => (
-                    <li className='contact-list-item'>
-                        <div className='contact-avatar'
-                        style={{background: `url(${contact.avatarURL})`}}>
-                        </div>
-                        <div className='contact-details'>
-                        <p>{contact.name}</p>
-                       <p>{contact.handle}</p>
-                       </div>
-                       <button className='contact-remove'>Remove</button>
-                    </li>
+const ListContacts = (props) => {
+    return(
+        <ol className = 'contact-list' >
+        {props.contacts.map((contact) => (
+            <li className='contact-list-item'>
+                <div className='contact-avatar'
+                style={{background: `url(${contact.avatarURL})`}}>
+                </div>
+                <div className='contact-details'>
+                <p>{contact.name}</p>
+               <p>{contact.handle}</p>
+               </div>
+               <button className='contact-remove'>Remove</button>
+            </li>
+            ))}
+         </ol>
 
-                ))}
-            </ol>
-        )
-    }
+    )
 }
-
-
-
-
-
 export default ListContacts
