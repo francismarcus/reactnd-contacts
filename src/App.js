ab4b98115abf51fd690c08ce3c54e612cc09d9c6
Marcus Francis <marcusmagnusson@outlook.com>
2018-11-27 02:10:22 +0100

    Feat: componentDidMount
    - Removed default state
    - Added lifecycle hook to fetch contacts from server and setState

--- src/App.js
+++ src/App.js
@@ -1,35 +1,21 @@
 import React, { Component } from 'react';
 import ListContacts from './ListContacts'
 
-// Default state, remove later.
-
+import * as ContactsAPI from './utils/ContactsAPI'
 
 class App extends Component {
   state = {
-    contacts: [
-    {
-     "id": "Salmon",
-      "name": "Salmon & Coral",
-      "handle": "@Snapchat",
-     "avatarURL": "http://localhost:5001/salmoncoral.png"
-    },
-    {
-     "id": "Celery",
-      "name": "Mr. Celery",
-      "handle": "@Celerysquash",
-     "avatarURL": "http://localhost:5001/celery.jpg"
-    },
-    {
-      "id": "Grey",
-     "name": "Mr. Grey",
-      "handle": "@Sage",
-      "avatarURL": "http://localhost:5001/grey.jpg"
-      },
-    ]
+    contacts: []
   }
 
+  componentDidMount() {
+    ContactsAPI.getAll().then((contacts) => {
+        this.setState(() => ({
+          contacts
+        }))
+      })
+  }
 
-//  Hook up delete button with onDeleteContact function
 removeContact = (contact) => {
   this.setState((currentState) => ({
     contacts: currentState.contacts.filter((c) => {
